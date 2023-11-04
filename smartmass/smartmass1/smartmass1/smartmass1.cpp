1c96be2d34ba5b4864d7f5cae06e400683743b12
Roman <Dronusv22@mail.ru>
2023-11-04 17:29:59 +0300

    add smartmass

---
+++ smartmass/smartmass1/smartmass1/smartmass1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <exception>
+using namespace std;
+
+class smart_array {
+private:
+    int* arr;
+    int maxsize = 0;
+    int index_size = 0;
+public:
+    smart_array(const int& size) {
+        maxsize = size;
+        arr = new int[size];
+    }
+    ~smart_array() {
+        delete[] arr;
+    }
+    void add_element(const int& element) {
+        if (index_size + 1 > maxsize) {
+            throw invalid_argument("oversize!");
+        }
+        arr[index_size] = element;
+        ++index_size;
+    }
+    int get_element(const int& index_element) {
+        if (index_element < 0 || index_element >= index_size) {
+            throw invalid_argument("wrong index!");
+        }
+        return arr[index_element];
+    }
+
+};
+
+int main()
+{
+    try {
+        smart_array arr(5);
+        arr.add_element(1);
+        arr.add_element(4);
+        arr.add_element(155);
+        arr.add_element(14);
+        arr.add_element(15);
+        std::cout << arr.get_element(1) << std::endl;
+    }
+    catch (const std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    return 0;
+}
---
+++ smartmass/smartmass2/smartmass2/smartmass2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <exception>
+using namespace std;
+
+class smart_array {
+private:
+    int* arr;
+    int maxsize = 0;
+    int index_size = 0;
+public:
+    smart_array(const int& size) {
+        maxsize = size;
+        arr = new int[size];
+    }
+    ~smart_array() {
+        delete[] arr;
+    }
+    void add_element(const int& element) {
+        if (index_size + 1 > maxsize) {
+            throw invalid_argument("oversize!");
+        }
+        arr[index_size] = element;
+        ++index_size;
+    }
+    int get_element(const int& index_element) {
+        if (index_element < 0 || index_element >= index_size) {
+            throw invalid_argument("wrong index!");
+        }
+        return arr[index_element];
+    }
+    smart_array& operator =(const smart_array& rhs) {
+        maxsize = rhs.maxsize;
+        index_size = rhs.index_size;
+        for (int i = 0; i < maxsize; ++i) {
+            arr[i] = rhs.arr[i];
+        }
+        return *this;
+    }
+};
+
+int main()
+{
+    smart_array arr(5);
+    arr.add_element(1);
+    arr.add_element(4);
+    arr.add_element(155);
+
+    smart_array new_array(2);
+    new_array.add_element(44);
+    new_array.add_element(34);
+
+    arr = new_array;
+    return 0;
+}
