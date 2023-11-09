89e618343e8fdd4cc2ec716105e29b3cb10f3eaa
Roman <Dronusv22@mail.ru>
2023-11-09 18:20:44 +0300

    Add hm

---
+++ newс++/stdfunction/stdfunction/stdfunction.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <functional>  
+#include <vector>
+int main()
+{
+   
+    std::vector<float> angles{ 30 * 3.1415926 / 180,60 * 3.1415926 / 180 ,90 * 3.1415926 / 180 };
+    std::vector<std::function<void(float)>> functions;
+    functions.push_back([](const float& a) {
+        float sin30 = 30 * 3.1415926 / 180;
+        float sin60 = 60 * 3.1415926 / 180;
+        float sin90 = 90 * 3.1415926 / 180;
+        if (a == sin30) {
+            std::cout << "sin: 0.5 ";
+        }
+        if (a == sin60) {
+            std::cout << "sin: 0.866025 ";
+        }
+        if (a == sin90) {
+            std::cout << "sin: 1 ";
+        }
+    });
+   functions.push_back([](float a) {
+       float cos30 = 30 * 3.1415926 / 180;
+       float cos60 = 60 * 3.1415926 / 180;
+       float cos90 = 90 * 3.1415926 / 180;
+       if (a == cos30) {
+           std::cout << "cos: 0.866025 ";
+       }
+       if (a == cos60) {
+           std::cout << "cos: 0.5 ";
+       }
+       if (a == cos90) {
+           std::cout << "cos: 2.67949e-08";
+       }
+       });
+
+    for (const auto& angle : angles) {
+        std::cout << angle << ": ";
+        for (const auto& function : functions)
+            function(angle);
+        std::cout << std::endl;
+    }
+}
+
---
+++ newс++/новшества задание1/новшества задание1/новшества задание1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <windows.h>
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    std::vector<int> v{ 4,7,9,14,12 };
+    std::cout << "Входные данные";
+    for (auto c : v) {
+        std::cout << c << " ";
+    }
+    std::cout << "\n";
+    std::cout << "Выходные данные ";
+    std::for_each(v.begin(), v.end(), [&](int& i) {
+        if (i % 2 != 0) {
+            i *= 3;
+        }
+        });
+    for (auto c : v) {
+        std::cout << c << " ";
+    }
+    return 0;
+}
+
