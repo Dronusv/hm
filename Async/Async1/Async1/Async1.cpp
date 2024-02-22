5eebf05d6d4c6e4938553870983cbdf856b579d4
Romchik <Dronusv22@mail.ru>
2024-02-22 09:53:13 +0300

    Add Async

---
+++ Async/Async1/Async1/Async1.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <vector>
+#include <iostream>
+#include <thread>
+#include <future>
+
+void sort(std::vector<int>::iterator& begin, std::vector<int>::iterator& end) {
+	
+
+}
+
+std::vector<int>::iterator mini(const std::vector<int>::iterator& begin, const std::vector<int>::iterator& end) {
+	std::vector<int>::iterator min = begin;
+	std::vector<int>::iterator ptr;
+	for (ptr = begin; ptr < end; ptr++) {
+		if (*ptr < *min) {
+			min = ptr;
+		}
+	}
+	return min;
+}
+
+int main()
+{
+	std::vector<int> v{ 5,3,1,2,4 };
+	std::promise<std::vector<int>::iterator> prom;
+	std::future<std::vector<int>::iterator> res = prom.get_future();
+	for (std::vector<int>::iterator ptr = v.begin(); ptr < v.end(); ptr++) {
+		auto f = std::async(mini,std::ref(ptr),v.end());
+		std::vector<int>::iterator m = f.get();
+		int temp = 0;
+		temp = *ptr;
+		*ptr = *m;
+		*m = temp;
+		
+	}
+
+
+	for (auto c : v) {
+		std::cout << c<<" ";
+	}
+}
+
---
+++ Async/async2/async2/async2.cpp
@@ -0,0 +1,42 @@
+﻿#include <algorithm>
+#include <future>
+#include <iostream>
+#include <mutex>
+#include <numeric>
+#include <string>
+#include <vector>
+
+
+int test(std::vector<int>::iterator begin, std::vector<int>::iterator end) {
+    int sum=0;
+    for (std::vector<int>::iterator ptr = begin; ptr < end; ptr++) {
+        sum += *ptr;
+    }
+    return sum;
+    
+}
+
+template<typename It,class F,size_t size = 5>
+auto forEach(It begin, It end,F func) {
+    size_t curr_size = std::distance(begin, end);
+    if (curr_size <= size) {
+        return func(begin,end);
+    }
+    auto mid = begin;
+    std::advance(mid, curr_size/2); 
+    auto fl_res=std::async(forEach<It,F>, begin, mid,func);
+    auto l_res= func(mid, end);
+    return fl_res.get() + l_res;
+   
+}
+
+
+
+int main()
+{
+    std::vector<int> v (20, 1);
+    int sum=forEach(v.begin(),v.end (),&test);
+    std::cout << sum;
+    
+}
+
