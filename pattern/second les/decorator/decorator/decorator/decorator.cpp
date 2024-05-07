b659449557e45fd35d7e7e1de76fce2de0a376cf
Romchik <Dronusv22@mail.ru>
2024-05-07 15:07:40 +0300

    Add proxy

---
+++ pattern/second les/decorator/decorator/decorator/decorator.cpp
@@ -0,0 +1,62 @@
+﻿#include <string>
+#include <iostream>
+#include <algorithm> 
+class Text {
+public:
+    virtual void render(const std::string& data) const {
+        std::cout << data;
+    }
+};
+
+
+class DecoratedText : public Text {
+public:
+    explicit DecoratedText(Text* text) : text_(text) {}
+    Text* text_;
+};
+
+class ItalicText : public DecoratedText {
+public:
+    explicit ItalicText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data)  const {
+        std::cout << "<i>";
+        text_->render(data);
+        std::cout << "</i>";
+    }
+};
+
+class BoldText : public DecoratedText {
+public:
+    explicit BoldText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const {
+        std::cout << "<b>";
+        text_->render(data);
+        std::cout << "</b>";
+    }
+};
+
+class Paragraph : public DecoratedText {
+public:
+    explicit Paragraph(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data)  const {
+        std::cout << "<p>";
+        text_->render(data);
+        std::cout << "</p>";
+    }
+};
+
+class Reversed : public DecoratedText {
+public:
+    explicit Reversed(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data)  const {
+        std::string str = data;
+        std::reverse(str.begin(), str.end());
+        text_->render(str);
+   
+    }
+};
+
+int main() {
+    auto text_block = new Reversed(new Text());
+    text_block->render("Hello world");
+}
