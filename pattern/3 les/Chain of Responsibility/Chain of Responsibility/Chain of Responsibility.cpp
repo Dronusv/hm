d1078eb847dd7750e3413620817320634fc049af
Romchik <Dronusv22@mail.ru>
2024-05-14 20:47:44 +0300

    Add pattern 3 les

---
+++ pattern/3 les/Chain of Responsibility/Chain of Responsibility/Chain of Responsibility.cpp
@@ -0,0 +1,131 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+#include <exception>
+
+enum class Type {Warning,Eror,FatalEror,Unknow};
+class LogMessage {
+public:
+    LogMessage(Type type, const std::string& message) : _type(type), _message(message) {};
+
+    Type type() const {
+        return _type;
+    };
+
+     std::string message() const {
+        return _message;
+    };
+private:
+    Type _type;
+    std::string _message;
+};
+
+
+class MessageHandler {
+public:
+    virtual MessageHandler& setNextHandler(MessageHandler* handler)=0;
+    virtual void handleRequest(LogMessage LM) {};
+};
+
+class FatalErrorHandler : public MessageHandler {
+private:
+    MessageHandler* nextHandler;
+public:
+    MessageHandler& setNextHandler(MessageHandler* handler) {
+        nextHandler = handler;
+        return *nextHandler;
+     }
+     void handleRequest(LogMessage LM) override
+     {
+         if (LM.type() == Type::FatalEror) {
+             throw LM.message();
+         }
+         else if (nextHandler != nullptr) {
+             nextHandler->handleRequest(LM);
+         }
+     }
+};
+
+class  ErrorHandler : public MessageHandler {
+private:
+    MessageHandler* nextHandler;
+public:
+    MessageHandler& setNextHandler(MessageHandler* handler) {
+        nextHandler = handler;
+        return *nextHandler;
+    }
+    void handleRequest(LogMessage LM) override
+    {
+        if (LM.type() == Type::Eror) {
+            std::ofstream out;
+            out.open("Log.txt");
+            if (out.is_open())
+            {
+                out << LM.message() << std::endl;
+            }
+            out.close();
+        }
+        else if (nextHandler != nullptr) {
+            nextHandler->handleRequest(LM);
+        }
+    }
+};
+
+class  WarningHandler : public MessageHandler {
+private:
+    MessageHandler* nextHandler;
+public:
+    MessageHandler& setNextHandler(MessageHandler* handler) {
+        nextHandler = handler;
+        return *nextHandler;
+    }
+    void handleRequest(LogMessage LM) override
+    {
+        if (LM.type() == Type::Warning) {
+            std::cout << LM.message();
+        }
+        else if (nextHandler != nullptr) {
+            nextHandler->handleRequest(LM);
+        }
+    }
+};
+class  UnknowHandler : public MessageHandler {
+private:
+    MessageHandler* nextHandler;
+public:
+    MessageHandler& setNextHandler(MessageHandler* handler) {
+        nextHandler = handler;
+        return *nextHandler;
+    }
+    void handleRequest(LogMessage LM) override
+    {
+        if (LM.type() == Type::Unknow) {
+            throw LM.message();
+        }
+        else if (nextHandler != nullptr) {
+            nextHandler->handleRequest(LM);
+        }
+    }
+};
+int main()
+{
+    try{
+         LogMessage LMW(Type::Warning, "War");
+         LogMessage LME(Type::Eror, "Error");
+         LogMessage LMFE(Type::FatalEror, "Fatal error");
+         LogMessage LMU(Type::Unknow, "Unknow");
+
+         FatalErrorHandler FEM;
+         ErrorHandler EH;
+         WarningHandler WH;
+         UnknowHandler UH;
+         FEM.setNextHandler(&EH).setNextHandler(&WH).setNextHandler(&UH);
+         
+         FEM.handleRequest(LMU);
+    }
+    catch (const std::string &ex) {
+        std::cout << ex;
+    }
+}
+
---
+++ pattern/3 les/comm/comm/comm.cpp
@@ -0,0 +1,151 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+
+class PrintCommand : public LogCommand {
+public:
+    void print(const std::string& message) {
+        std::cout << message;
+    }
+};
+
+class  FstrCommand:public LogCommand {
+public:
+    void print(const std::string& message) {
+        std::ofstream out;
+        out.open("File.txt");
+        if (out.is_open())
+        {
+            out << message << std::endl;
+        }
+        out.close();
+    }
+};
+
+void print(LogCommand& LC, const std::string& message) {
+    LC.print(message);
+}
+
+
+class SomeClass;
+
+
+class Observer {
+public:
+    
+    Observer(SomeClass& sclass);
+    
+    virtual void onWarning(const std::string& message) {}
+    virtual void onError(const std::string& message) {}
+    virtual void onFatalError(const std::string& message) {}
+    
+protected:
+    SomeClass& SC;
+
+};
+
+
+
+class SomeClass {
+public:
+    void warning(const std::string& message) const {
+        for (Observer* observer : observers) {
+            observer->onWarning(message);
+        }
+    };
+    void error(const std::string& message) const {
+        for (Observer* observer : observers) {
+            observer->onError(message);
+        }
+    };
+    void fatalError(const std::string& message) const {
+        for (Observer* observer : observers) {
+            observer->onFatalError(message);
+        }
+    };
+    void registerObserver(Observer* observer) {
+        observers.push_back(observer);
+    }
+    void removeObserver(Observer* observer) {
+        auto it = std::remove(observers.begin(), observers.end(), observer);
+        observers.erase(it, observers.end());
+    }
+    
+private:
+    std::vector<Observer*> observers;
+};
+
+Observer::Observer(SomeClass& sclass) :SC(sclass) {
+    SC.registerObserver(this);
+}
+
+class ObserverWarning :public Observer {
+public:
+    using Observer::Observer;
+    void onWarning(const std::string& message) {
+        std::cout << message;
+    }
+    ~ObserverWarning() {
+        SC.removeObserver(this);
+    }
+};
+class ObserverError :public Observer {
+public:
+    using Observer::Observer;
+    void onError(const std::string& message) {
+        std::fstream out;
+        out.open("File.txt",std::ofstream::app);
+        if (out.is_open())
+        {
+            out << message << "\n" << std::endl;
+        }
+        
+    }
+    ~ObserverError() {
+        SC.removeObserver(this);
+    }
+};
+class ObserverFatalError :public Observer {
+public:
+    using Observer::Observer;
+    void onFatalError(const std::string& message) {
+        std::cout << message;
+        std::fstream out;
+        out.open("File.txt", std::ofstream::app);
+        if (out.is_open())
+        {
+            out << message<<"\n" << std::endl;
+        }
+       
+    }
+    ~ObserverFatalError() {
+        SC.removeObserver(this);
+    }
+    
+};
+
+
+
+    
+int main()
+{
+    std::cout << "Hello World!\n";
+    SomeClass SC;   
+    ObserverError ob(SC);
+    ObserverWarning ow(SC);
+    ObserverFatalError ofe(SC);
+    SC.error("eror");
+    SC.warning("warning");
+    SC.fatalError("ATAL EROR");
+    
+    
+}
+
