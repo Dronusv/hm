677bcd436d447d9c88ab8f0a4de213a13c92a639
Romchik <Dronusv22@mail.ru>
2024-07-17 10:44:54 +0300

    add QT

---
+++ Qt/lsn2/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_lsn2_FILE C:/Users/PC/Desktop/hm/Qt/lsn2/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/lsn2.exe)
+set(__QT_DEPLOY_TARGET_lsn2_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_lsn2_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
